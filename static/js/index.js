dc5f3d13da015b69d8a0ad2d81fa535dbefde37a
Omri Avrahami <omri.avrahami@gmail.com>
2022-11-29 13:19:39 +0200

    Make auto-scrolling slower.

--- static/js/index.js
+++ static/js/index.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     slidesToShow: 4,
     slidesToScroll: 4,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 4000,
     responsive: [
       {
         breakpoint: 1024,
